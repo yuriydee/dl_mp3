72257b43fc9f851f680e158cb398e91d93041f9e
Yuriy Denysov <ydenysov@scholastic.com>
2017-03-13 09:57:03 -0400

    works for firefox

--- get_songs.py
+++ get_songs.py
@@ -2,14 +2,30 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-i=1
-while (i < 153):
-    driver = webdriver.Firefox()
-    driver.get("https://soundtake.net/#https://soundcloud.com/yuriydee/likes")#put here the adress of your page
+#i=1
+#while (i < 153):
+timeout = 10
+browser = webdriver.Firefox()
+browser.get("https://soundtake.net/#https://soundcloud.com/yuriydee/likes")#put here the adress of your page
     #elem = driver.find_elements_by_xpath("//*[@id="kanan"]/div[2]/div[2]/div[4]/a")#put here the content you have put in Notepad, ie the XPath
+#browser.waitForElementPresent('//*[@id="kanan"]/div[2]/div[2]/div[4]/a')
+try:
+    element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="kanan"]/div[152]/div[2]/div[4]/a'))
+    WebDriverWait(browser, timeout).until(element_present)
     elem = browser.find_element_by_xpath('//*[@id="kanan"]/div[2]/div[2]/div[4]/a')
-    #print(elem .get_attribute("class"))
     elem.click()
-    i = i+1
-    driver.close()
+    browser.close()
+except TimeoutException:
+    print("Timed out waiting for page to load")
+
+    #print(elem .get_attribute("class"))
+
+#    i = i+1
+
+
+### //*[@id="kanan"]/div[2]/div[2]/div[4]/a
